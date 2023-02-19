7d3e9fc27e8ed99c377ffa15972265c436518576
vasilyl1 <likhovaido@gmail.com>
2023-02-19 14:44:51 -0500

    OOP functionality

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const mysql = require('mysql2'); // import and require mysql2
 require('dotenv').config(); // define and read env configuration
 const {department,role,employee} = require('./lib/classes'); // defines the current record as objects
 const CMS_init = require ('./lib/inq'); // user interview functionality
-const active_record = { // active selected record
+const active_request = { // the request the user is working on
     department : new department,
     role : new role,
     employee : new employee
 };
 
 // Connect to database
-const db = mysql.createConnection(
+active_request.department.db = mysql.createConnection(
   {
     host: process.env.DBhost,
     // MySQL username,
@@ -22,4 +22,4 @@ const db = mysql.createConnection(
 );
 
 // start CMS system
-CMS_init(db);
+CMS_init(active_request);
