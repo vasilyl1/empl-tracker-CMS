072de8cac9d3e0b5123b2ac9594e317a71d0424f
vasilyl1 <likhovaido@gmail.com>
2023-02-19 23:23:32 -0500

    async functions

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-const mysql = require('mysql2'); // import and require mysql2
-require('dotenv').config(); // define and read env configuration
-const {department,role,employee} = require('./lib/classes'); // defines the current record as objects
-const CMS_init = require ('./lib/inq'); // user interview functionality
+const { department, role, employee } = require('./lib/classes'); // defines the current record as objects
+const CMS_init = require('./lib/inq'); // user interview functionality
 const active_request = { // the request the user is working on
-    department : new department,
-    role : new role,
-    employee : new employee
+  department: new department,
+  role: new role,
+  employee: new employee
 };
-// Connect to database
-active_request.department.db = mysql.createConnection(
-  {
-    host: process.env.DBhost,
-    // MySQL username,
-    user: process.env.DBuser,
-    // MySQL password here
-    password: process.env.DBpassword,
-    database: process.env.DBdatabase
-  },
-  console.log(`Connected to ${process.env.DBdatabase} database, user ${process.env.DBuser}`)
-);
-active_request.role.db = active_request.department.db;
-active_request.employee.db = active_request.department.db;
 
-// start CMS system
-CMS_init(active_request);
+active_request.department.init().then(() => { // initialize objects and connect to database
+  active_request.role.db = active_request.department.db;
+  active_request.employee.db = active_request.department.db;
+  CMS_init(active_request); // start CMS system
+});
+
